4aca85b38837491295402bf7ceba048bf466aa6a
Keshawn287 <60359683+Keshawn287@users.noreply.github.com>
2021-08-07 22:17:50 -0400

    App.js and app.css updates
    Added background to app, made list and add button functional. Deleted component folders.

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,25 +1,69 @@
 //import logo from './logo.svg';
-import React,{ useState } from 'react';
+import React from 'react';
 import './App.css';
-import TodoForm from './components/TodoForm'; //pass as a prop
 
-function App() {
+//create a class component
 
-  const [todos, setTodos] = useState([]);
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+       text:'',
+       key:'' 
 
-  //takes in todo and adds it to an array of todos
-  function addTodo(todo){
-    setTodos([todo, ...todos]);
+      }
+    }
+    //bind key word to constructor
+    this.handleInput = this.handleInput.bind(this);
+    //bind key word to constructor
+    this.addItem = this.addItem.bind(this);
+  }
+  //handleInput and addItems functions adds items to To-do List
+  //called when you type input into field
+  handleInput(e){
+    //change state variable
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>React Todo</p>
-        <TodoForm addTodo={addTodo} />
-      </header>
-    </div>
-  );
+  //called when you click add button
+  addItem(e){
+    e.preventDefault(); //prevents page refreshing when you click add button
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    //check if newItem.text is empty if not add to list
+    if(newItem.text!==""){
+      const newItems=[...this.state.items, newItems]; //destructuring assignment 
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })   
+    }
+  }
+
+  render(){
+    return (
+        <div className="App">
+          <header>
+            <form id="to-do-form">
+              <input type="text" placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}/>
+              <button type="sumbit">Add</button>
+            </form>
+        </header>
+        </div>
+    );
+  }
 }
 
 export default App;
